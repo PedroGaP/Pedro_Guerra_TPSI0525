0cae0ea6b69b8cb37635198170f38de920ba521b
PedroGaP <pedroga.personal@gmail.com>
2025-07-09 17:31:12 +0100

    feat: Added a table to index.html and removed javascript features

--- Trabalho_1/js/index.js
+++
@@ -1,7 +0,0 @@
-let nomeInput = document.getElementById("nome");
-let nomeDiv = document.getElementById("nome_display");
-
-nomeInput.addEventListener("input", (ev) => {
-  console.log(nomeInput.value);
-  nomeDiv.innerHTML = `<p>${nomeInput.value}</p>`;
-});
